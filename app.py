ce50823a220aa352a084038ad6c88510a18af1e3
vani arora <23f3001116@ds.study.iitm.ac.in>
2026-07-06 00:31:30 +0530

    profile page added

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from werkzeug.security import  generate_password_hash,check_password_hash
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -42,7 +44,6 @@ class Trek(db.Model):
     start_date= db.Column(db.Date())
     end_date= db.Column(db.Date())
 
-
 class Booking(db.Model):
     __tablename__="booking"
     booking_id= db.Column(db.Integer(), primary_key=True)
@@ -59,24 +60,139 @@ class AssignedTreks(db.Model):
     trek_id= db.Column(db.Integer(), db.ForeignKey('trekking.trek_id'))
     assigned_date= db.Column(db.DateTime, default=datetime.utcnow)
 
+
+def auth_required(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        if 'user_id' in session:
+            return func(*args, **kwargs)
+        else:
+            flash('You are not logged in to the page!')
+            return redirect(url_for('login'))
+    return inner
+
 @app.route('/')
+@auth_required
 def index():
     return render_template("index.html", var1="VANI's")
-    
 
 @app.route('/login')
 def login():
     return render_template('login.html', var1="VANI")
 
+@app.route('/login', methods=['POST'])
+def login_post():
+    email=request.form.get('email')
+    password= request.form.get('password')
+    if not email or not password:
+        flash('Please fill out all the details')
+        return redirect(url_for('login'))
+    user= User.query.filter_by(email=email).first()
+    if not user:
+        flash("User doesnot exists")
+        return redirect(url_for('login'))
+    if not check_password_hash(user.passhash, password):
+        flash('Invalid password')
+        return redirect(url_for('login'))
+    if user.role == "Staff" and not user.approved:
+        flash("Your account is awaiting admin approval.")
+        return redirect(url_for('login'))
+
+    session['user_id'] = user.id
+    flash('Login Successful!')
+    return redirect(url_for('index'))
+
 @app.route('/register')
 def register():
     return render_template('register.html')
 
+@app.route('/register', methods=['POST'])
+def register_post():
+    fullname= request.form.get('fullname')
+    email= request.form.get('email')
+    password= request.form.get('password')
+    confirm_password= request.form.get('confirm_password')
+    contact = request.form.get('contact')
+    role = request.form.get('role')
+    
+    if not fullname or not email or not password or not confirm_password or not role:
+        flash('Please fill out all the details')
+        return redirect(url_for('register'))
+    if password != confirm_password:
+        flash("Password donot match")
+        return redirect(url_for('register'))
+    user= User.query.filter_by(email=email).first()
+
+    if user:
+        flash('User with this email already exists')
+        return redirect(url_for('register'))
+    
+    password_hash= generate_password_hash(password)
+    new_user= User(username=email, email=email, passhash=password_hash, name=fullname, contact=contact, role=role)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('login'))
+
+@app.route('/profile')
+@auth_required
+def profile():
+    user= User.query.get(session['user_id'])
+    return render_template("profile.html", user=user)
+
+@app.route('/profile', methods=['POST'])
+@auth_required
+def edit_profile():
+    username= request.form.get('username')
+    cpassword= request.form.get('cpassword')
+    password= request.form.get('password')
+    name= request.form.get('name')
+    email= request.form.get('email')
+
+    if not username or not cpassword or not password or not name or not email:
+        flash('Please fill all the details!')
+        return redirect(url_for('profile'))
+    
+    user= User.query.get(session['user_id'])
+    if not check_password_hash(user.passhash, cpassword):
+        flash('Incorrect password')
+        return redirect(url_for('profile'))
+    
+    if username != user.username:
+        new_username= User.query.filter_by(username=username).first()
+        if new_username:
+            flash('User already exists!')
+            return redirect(url_for('profile'))
+    if email != user.email:
+        new_email= User.query.filter_by(email=email).first()
+        if new_email:
+            flash('User already exists!')
+            return redirect(url_for('profile'))
+        
+    new_password_hash= generate_password_hash(password)
+    user.username= username
+    user.email= email
+    user.passhash= new_password_hash
+    user.name= name
+    db.session.commit()
+    flash('Profile updated successfully!')
+    return redirect(url_for('profile'))
+
+@app.route('/logout')
+@auth_required
+def logout():
+    session.pop('user_id')
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
+        admin= User.query.filter_by(is_admin=True).first()
+        if not admin:
+            password_hash= generate_password_hash('admin')
+            admin= User(username='admin@gmail.com', email='admin@gmail.com',passhash=password_hash, name='Admin', contact='9876543210', role='Admin', approved=True, is_admin=True)
+            db.session.add(admin)
+            db.session.commit()
     app.run(debug=True)
     
     
